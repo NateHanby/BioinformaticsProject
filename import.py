d267ee765b30c04b5d9249ffd23db1ef7bf4117c
NateHanby <110919689+NateHanby@users.noreply.github.com>
2023-11-29 10:37:35 -0600

    Update import.py

--- import.py
+++ import.py
@@ -1,4 +1,11 @@
 import urllib.request
+import gzip
+import shutil
 
-urllib.request.urlretrieve("ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/1000G_2504_high_coverage/working/20201028_3202_raw_GT_with_annot/20201028_CCDG_14151_B01_GRM_WGS_2020-08-05_chr12.recalibrated_variants.vcf.gz", 
-                           'child_chr_12.gz')
+#I don't know why this doesn't work. I ended up just downloading the file manually with an FileZilla FTP client and then unzipping it in windows!
+urllib.request.urlretrieve("ftp://ftp.1000genomes.ebi.ac.uk//vol1/ftp/release/20130502/supporting/hd_genotype_chip/ALL.chip.omni_broad_sanger_combined.20140818.snps.genotypes.vcf.gz", 
+                           'ALL.chip.omni_broad_sanger_combined.20140818.snps.genotypes.vcf.gz')
+
+with gzip.open('ALL.chip.omni_broad_sanger_combined.20140818.snps.genotypes.vcf.gz', 'rb') as zipped_file:
+     with open('file.txt', 'wb') as new_file:
+        shutil.copyfileobj(zipped_file, new_file)
